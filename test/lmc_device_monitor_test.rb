1f31aa54d4a4b4ffb0ac6afaf7023c5f1c5bc07c
erpel <philipp@copythat.de>
2019-05-16 00:10:44 +0200

    Inflate test coverage

--- test/lmc_device_monitor_test.rb
+++ test/lmc_device_monitor_test.rb
@@ -5,10 +5,12 @@ class LmcDeviceMonitorTest < Minitest::Test
   def test_device_monitor_data
     mock_lmc = Fixtures.mock_lmc
     mock_lmc.expect :get, Fixtures.test_response({}), [Array, Hash]
+    mock_lmc.expect :get, Fixtures.test_response({}), [Array, Hash]
     account = Fixtures.test_account mock_lmc
     device = Fixtures.test_device(account)
-      r = device.record 'uptime'
-      r.scalar 'datapoint', 144, 'MINUTE10'
+    r = device.record 'uptime'
+    r.scalar 'datapoint', 144, 'MINUTE10'
+    r.row 'dunno', 12, 'DAY'
     assert_mock mock_lmc
 
   end
